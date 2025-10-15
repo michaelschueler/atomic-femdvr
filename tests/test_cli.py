3ac31119164a3db859e97416af3fd0fab54c4dfd
Edward Linscott <edwardlinscott@gmail.com>
2025-10-15 16:02:41 +0200

    Extend CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,3 +6,17 @@ def test_cli_help(script_runner):
     assert result.success
     assert result.stdout.startswith("Usage:")
     assert ("atomic_femdvr [OPTIONS]") in result.stdout
+
+def test_atomic_cli_help(script_runner):
+    """Test `atomic_femdvr atomic --help`."""
+    result = script_runner.run(["atomic_femdvr", "atomic", "--help"])
+    assert result.success
+    assert result.stdout.startswith("Usage:")
+    assert ("atomic_femdvr atomic [OPTIONS]") in result.stdout
+
+def test_pseudoatomic_cli_help(script_runner):
+    """Test `atomic_femdvr pseudoatomic --help`."""
+    result = script_runner.run(["atomic_femdvr", "pseudoatomic", "--help"])
+    assert result.success
+    assert result.stdout.startswith("Usage:")
+    assert ("atomic_femdvr pseudoatomic [OPTIONS]") in result.stdout
